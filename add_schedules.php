a225d32ea349baa7d5ca86420bf1c62c84f9a6c3
WhiteRabbitGeek <beckyemunn@gmail.com>
2018-07-07 11:24:11 +0100

    quick save before changes

--- add_schedules.php
+++ add_schedules.php
@@ -18,110 +18,221 @@ try
         $dateFrom = $_GET['from'];
         $dateTo = $_GET['to'];
 
-        $sql_date_range = "SELECT date FROM `schedules` WHERE date BETWEEN '$dateFrom' and '$dateTo' ORDER BY date";
+        $sql_date_range = "SELECT date FROM `schedules` WHERE date BETWEEN '$dateFrom' and '$dateTo' ORDER BY date ASC";
         $date_range = $pdo->query($sql_date_range);
-        $sql_sound_console_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '1' ORDER BY last_used";
+        $sql_sound_console_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '1' ORDER BY last_used DESC";
         $sound_console_last_used = $pdo->query($sql_sound_console_last_used);
-        $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used";
+        $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used DESC";
         $mics_last_used = $pdo->query($sql_mics_last_used);
-        $sql_platform_mic_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '3' ORDER BY last_used";
+        $sql_platform_mic_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '3' ORDER BY last_used DESC";
         $platform_mic_last_used = $pdo->query($sql_platform_mic_last_used);
-        $sql_car_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '4' ORDER BY last_used";
+        $sql_car_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '4' ORDER BY last_used DESC";
         $car_watch_last_used = $pdo->query($sql_car_watch_last_used);
-        $sql_door_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '5' ORDER BY last_used";
+        $sql_door_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '5' ORDER BY last_used DESC";
         $door_watch_last_used = $pdo->query($sql_door_watch_last_used);
 
 //--------------------------------------------- Create schedule
         while ($row = $date_range ->fetch())
         {
+            $used_for_this_date = array();
             $date_row = $row['date'];
 
             //--------------------------------------------- Sound Console
+            $found = "0";
+//            $sql_sound_console_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '1' ORDER BY last_used DESC";
+//            $sound_console_last_used = $pdo->query($sql_sound_console_last_used);
             $row = $sound_console_last_used ->fetch();
+            $vol_id = $row['vol_id'];
             if ($row == NULL) {
-                $sql_sound_console_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '1' ORDER BY last_used";
+                $sql_sound_console_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '3' ORDER BY last_used DESC";
                 $sound_console_last_used = $pdo->query($sql_sound_console_last_used);
                 $row = $sound_console_last_used ->fetch();
-            }
-            $vol_id = $row['vol_id'];
-            $sql_update_sound = "UPDATE schedules SET sound_console = $vol_id WHERE date = '$date_row'";
+                $vol_id = $row['vol_id'];
+             }
+            $sql_update_sound = "UPDATE schedules SET sound_console = '$vol_id' WHERE date = '$date_row'";
             $update_sound = $pdo->query($sql_update_sound);
-            $sql_update_sound_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 1";
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_sound_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 1";
             $update_sound_last_used = $pdo->query($sql_update_sound_last_used);
 
-        //--------------------------------------------- Mics
-            $row = $mics_last_used ->fetch();
-            if ($row == NULL) {
-                $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used";
-                $mics_last_used = $pdo->query($sql_mics_last_used);
-                $row = $mics_last_used ->fetch();
-            }
+            //--------------------------------------------- Platform Mic
+            $found = "0";
+//            $sql_platform_mic_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '3' ORDER BY last_used DESC";
+//            $platform_mic_last_used = $pdo->query($sql_platform_mic_last_used);
+            $row = $platform_mic_last_used ->fetch();
             $vol_id = $row['vol_id'];
-            $sql_update_mics = "UPDATE schedules SET platform_mic = $vol_id WHERE date = '$date_row'";
-            $update_mics = $pdo->query($sql_update_mics);
-            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 3";
-            $update_mics_last_used = $pdo->query($sql_update_mics_last_used);
+            if ($row == NULL) {
+                $sql_platform_mic_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '3' ORDER BY last_used";
+                $platform_mic_last_used = $pdo->query($sql_platform_mic_last_used);
+                $row = $platform_mic_last_used ->fetch();
+                $vol_id = $row['vol_id'];
+             }
 
+            while ($found != "1"){
+                if (in_array("$vol_id", $used_for_this_date))
+                {
+                    $row = $platform_mic_last_used ->fetch();
+                    $vol_id = $row['vol_id'];
+                    if ($row == NULL) {
+                        $sql_platform_mic_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '3' ORDER BY last_used DESC";
+                        $platform_mic_last_used = $pdo->query($sql_platform_mic_last_used);
+                        $row = $platform_mic_last_used ->fetch();
+                        $vol_id = $row['vol_id'];
+                     }
+                  $found = "0";
+                }
+                else if (!in_array('$vol_id', $used_for_this_date)) {
+                    $found = "1";
+                }
+            }
+
+            $sql_update_plat_mics = "UPDATE schedules SET platform_mic = '$vol_id' WHERE date = '$date_row'";
+            $update_plat_mics = $pdo->query($sql_update_plat_mics);
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_plat_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 3";
+            $update_plat_mics_last_used = $pdo->query($sql_update_plat_mics_last_used);
+
+
+            //--------------------------------------------- Mics
+            $found = "0";
             $row = $mics_last_used ->fetch();
             if ($row == NULL) {
-                $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used";
+                $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used DESC";
                 $mics_last_used = $pdo->query($sql_mics_last_used);
                 $row = $mics_last_used ->fetch();
             }
             $vol_id = $row['vol_id'];
+            while ($found != "1"){
+                if (in_array("$vol_id", $used_for_this_date))
+                {
+                    $row = $mics_last_used ->fetch();
+                    $vol_id = $row['vol_id'];
+                    if ($row == NULL) {
+                        $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used DESC";
+                        $mics_last_used = $pdo->query($sql_mics_last_used);
+                        $row = $platform_mic_last_used ->fetch();
+                        $vol_id = $row['vol_id'];
+                     }
+                  $found = "0";
+                }
+                else {
+                    $found = "1";
+                }
+            }
             $sql_update_mics = "UPDATE schedules SET windows_mic = $vol_id WHERE date = '$date_row'";
             $update_mics = $pdo->query($sql_update_mics);
-            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 2";
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 2";
             $update_mics_last_used = $pdo->query($sql_update_mics_last_used);
 
+
+            $found = "0";
             $row = $mics_last_used ->fetch();
-            if ($row == NULL) {
-                $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used";
-                $mics_last_used = $pdo->query($sql_mics_last_used);
-                $row = $mics_last_used ->fetch();
-            }
             $vol_id = $row['vol_id'];
+            while ($found != "1"){
+                if (in_array("$vol_id", $used_for_this_date))
+                {
+                    $row = $mics_last_used ->fetch();
+                    $vol_id = $row['vol_id'];
+                    if ($row == NULL) {
+                        $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used DESC";
+                        $mics_last_used = $pdo->query($sql_mics_last_used);
+                        $row = $platform_mic_last_used ->fetch();
+                        $vol_id = $row['vol_id'];
+                     }
+                  $found = "0";
+                }
+                else {
+                    $found = "1";
+                }
+            }
             $sql_update_mics = "UPDATE schedules SET centre_mic = $vol_id WHERE date = '$date_row'";
             $update_mics = $pdo->query($sql_update_mics);
-            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 2";
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 2";
             $update_mics_last_used = $pdo->query($sql_update_mics_last_used);
 
+
+            $found = "0";
             $row = $mics_last_used ->fetch();
-            if ($row == NULL) {
-                $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used";
-                $mics_last_used = $pdo->query($sql_mics_last_used);
-                $row = $mics_last_used ->fetch();
-            }
             $vol_id = $row['vol_id'];
+            while ($found != "1"){
+                if (in_array("$vol_id", $used_for_this_date))
+                {
+                    $row = $mics_last_used ->fetch();
+                    $vol_id = $row['vol_id'];
+                    if ($row == NULL) {
+                        $sql_mics_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '2' ORDER BY last_used DESC";
+                        $mics_last_used = $pdo->query($sql_mics_last_used);
+                        $row = $platform_mic_last_used ->fetch();
+                        $vol_id = $row['vol_id'];
+                     }
+                  $found = "0";
+                }
+                else {
+                    $found = "1";
+                }
+            }
             $sql_update_mics = "UPDATE schedules SET notice_boards_mic = $vol_id WHERE date = '$date_row'";
             $update_mics = $pdo->query($sql_update_mics);
-            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 2";
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_mics_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 2";
             $update_mics_last_used = $pdo->query($sql_update_mics_last_used);
 
+
             //--------------------------------------------- Car Watch
+            $found = "0";
             $row = $car_watch_last_used ->fetch();
-            if ($row == NULL) {
-                $sql_car_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '4' ORDER BY last_used";
-                $car_watch_last_used = $pdo->query($sql_car_watch_last_used);
-                $row = $car_watch_last_used ->fetch();
-            }
             $vol_id = $row['vol_id'];
+            while ($found != "1"){
+                if (in_array("$vol_id", $used_for_this_date))
+                {
+                    $row = $car_watch_last_used ->fetch();
+                    $vol_id = $row['vol_id'];
+                    if ($row == NULL) {
+                        $sql_car_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '4' ORDER BY last_used DESC";
+                        $car_watch_last_used = $pdo->query($sql_car_watch_last_used);
+                        $row = $car_watch_last_used ->fetch();
+                        $vol_id = $row['vol_id'];
+                     }
+                  $found = "0";
+                }
+                else {
+                    $found = "1";
+                }
+            }
             $sql_update_sound = "UPDATE schedules SET car_watch = $vol_id WHERE date = '$date_row'";
             $update_sound = $pdo->query($sql_update_sound);
-            $sql_update_car_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 4";
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_car_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 4";
             $update_car_last_used = $pdo->query($sql_update_car_last_used);
 
+
             //--------------------------------------------- Door Watch
+            $found = "0";
             $row = $door_watch_last_used ->fetch();
-            if ($row == NULL) {
-                $sql_door_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '5' ORDER BY last_used";
-                $door_watch_last_used = $pdo->query($sql_door_watch_last_used);
-                $row = $door_watch_last_used ->fetch();
-            }
             $vol_id = $row['vol_id'];
+            while ($found != "1"){
+                if (in_array("$vol_id", $used_for_this_date))
+                {
+                    $row = $door_watch_last_used ->fetch();
+                    $vol_id = $row['vol_id'];
+                    if ($row == NULL) {
+                        $sql_door_watch_last_used = "SELECT vol_id FROM `volunteer_roles` WHERE role_id = '5' ORDER BY last_used DESC";
+                        $door_watch_last_used = $pdo->query($sql_door_watch_last_used);
+                        $row = $door_watch_last_used ->fetch();
+                        $vol_id = $row['vol_id'];
+                     }
+                  $found = "0";
+                }
+                else {
+                    $found = "1";
+                }
+            }
             $sql_update_door = "UPDATE schedules SET door_watch = $vol_id WHERE date = '$date_row'";
             $update_door = $pdo->query($sql_update_door);
-            $sql_update_door_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE $vol_id and role_id = 5";
+            array_push($used_for_this_date, $vol_id);
+            $sql_update_door_last_used = "UPDATE volunteer_roles SET last_used = '$date_row' WHERE vol_id = '$vol_id' and role_id = 5";
             $update_door_last_used = $pdo->query($sql_update_door_last_used);
         }
 //--------------------------------------------- End
